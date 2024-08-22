1718635dbaaac660f59898be7c8757757ed90957
Josh <joshmono2008@outlook.com>
2024-08-22 22:05:57 +1000

    Simple World Generation

--- Homework-Task-OOP/Homework_Task_OOP.py
+++ Homework-Task-OOP/Homework_Task_OOP.py
@@ -1,8 +1,17 @@
 import pygame
+import random
+
 
 pygame.init()
-screen = pygame.display.set_mode([500, 500])
+screen_width = 512
+screen_height = 512
+screen = pygame.display.set_mode([screen_width, screen_height])
 screen.fill((0, 0, 0))
+
+
+###
+### Player
+###
 
 class PlayerSprite(pygame.sprite.Sprite):
     def __init__(self):
@@ -42,16 +51,104 @@ class Player(PlayerSprite):
         else:
             self.health -= damage            
 
+
+###
+### Blocks
+###
+
+class Block:
+    def __init__(self, durablity: int, blast_protection: int, gravity_effected: bool, x: int, y: int):
+        self.durablity = durablity
+        self.health = durablity
+        self.blast_protection = blast_protection
+        self.gravity_effected = gravity_effected
+        self.destroyed = False
+        self.x = x
+        self.y = y
+
+    def take_damage(self, damage: int, damage_type: str):
+
+        if damage_type == "TNT":
+            damage /= self.blast_protection
+            if self.health > damage:
+                self.health -= damage
+            else:
+                self.health = 0
+                self.destroyed = True
+        elif damage_type == "Player":
+            if self.health > damage:
+                self.health -= damage
+            else:
+                self.health = 0
+                self.destroyed = True
+
+        print(F"Damage: {damage}")
+        print(F"Health: {self.health}")
+
+block_dict = {
+        1 : pygame.image.load("iron.png"),
+        2 : pygame.image.load("dirt.png"),
+        3 : pygame.image.load("obsidian.png"),
+        4 : pygame.image.load("sand.png")
+    }
+
+class Iron(Block):
+    def __init__(self, x: int, y: int):
+        super().__init__(10, 5, False, x, y)
+        self.image = block_dict[1]
+        
+class Dirt(Block):
+    def __init__(self, x: int, y: int):
+        super().__init__(5, 1, False, x, y)
+        self.image = block_dict[2]
+
+class Obsidian(Block):
+    def __init__(self, x: int, y: int):
+        super().__init__(50, 30, False, x, y)
+        self.image = block_dict[3]
+
+class Sand(Block):
+   def __init__(self, x: int, y: int):
+        super().__init__(5, 1, True, x, y)
+        self.image = block_dict[4]
+
+
+all_blocks = [Iron, Dirt, Obsidian, Sand]
+
+
+block_list = []
+def generate_world(block_list: list):
+    block_list = []
+    for x in range(round(screen_width/64)):
+        for y in range(round(screen_height/64)):
+            block = all_blocks[random.randint(0,len(all_blocks)-1)]
+            block_list.append(block(x*64, y*64))
+
+    return block_list
+
+    
+
+
+
+
+
+
+            
 clock = pygame.time.Clock()
    
+block_list = generate_world(block_list)
+
 player = Player()
 running = True
 while running:
+    
     clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
+            elif event.key == pygame.K_SPACE:
+                block_list = generate_world(block_list)
         
         elif event.type == pygame.QUIT:
             running = False
@@ -61,5 +158,7 @@ while running:
     player.update(pressed_keys)
     pygame.display.flip()
     screen.fill((0, 0, 0))
-    
+    for block in block_list:
+        screen.blit(block.image, (block.x, block.y))
+   
 pygame.quit()
